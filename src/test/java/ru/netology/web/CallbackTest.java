d19665b5e5d500b6d9822c372c7bdecb9c273783
3Gouct <ilya_football@mail.ru>
2022-08-07 20:31:32 +0300

    changes test

--- src/test/java/ru/netology/web/CallbackTest.java
+++ src/test/java/ru/netology/web/CallbackTest.java
@@ -6,10 +6,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
