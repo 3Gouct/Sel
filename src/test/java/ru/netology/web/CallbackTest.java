edea948838128eb3ee707df22bea99f4fd4bb244
3Gouct <ilya_football@mail.ru>
2022-08-07 21:32:17 +0300

    chenges gradle

--- src/test/java/ru/netology/web/CallbackTest.java
+++ src/test/java/ru/netology/web/CallbackTest.java
@@ -16,13 +16,13 @@ class CallbackTest {
     private WebDriver driver;
 
     @BeforeAll
-    static void setUpAll () {
+    public static void setUpAll () {
     System.setProperty ("webdriver.chrome.driver", "driver/win/chromedriver.exe");
     }
 
 
     @Test
-    void test () {
+    public void test () {
         driver.get ("http://localhost:9999");
         driver.findElement (By.cssSelector ("span[data-test-id='name'] input")).sendKeys ("Иванов Иван");
         driver.findElement (By.cssSelector ("span[data-test-id='phone'] input")).sendKeys ("+79999999999");
@@ -33,7 +33,7 @@ class CallbackTest {
     }
 
     @Test
-    void test1 () {
+    public void test1 () {
         driver.get ("http://localhost:9999");
         driver.findElement (By.cssSelector ("span[data-test-id='name'] input")).sendKeys ("Иванова-Петрова Наталья");
         driver.findElement (By.cssSelector ("span[data-test-id='phone'] input")).sendKeys ("+79999999999");
@@ -44,17 +44,18 @@ class CallbackTest {
     }
 
     @BeforeEach
-    void setUp () {
+    public void setUp () {
 
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
         driver = new ChromeDriver(options);
+        driver.get ("http://localhost:9999");
     }
 
     @AfterEach
-    void tearDown() {
+    public void tearDown() {
         driver.quit ();
         driver=null;
     }
